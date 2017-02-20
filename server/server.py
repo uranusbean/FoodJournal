afa1cde6de1dd0b8e93029aab41db1b793c22c1a
Yifan Sun <sunyifan112358@gmail.com>
2017-02-19 22:16:53 -0500

    Changed folder structure

--- server/server.py
+++ server/server.py
@@ -4,5 +4,5 @@ import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
 httpd = BaseHTTPServer.HTTPServer(('', 8080), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='../server/server.pem', server_side=True)
 httpd.serve_forever()
