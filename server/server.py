a35112c06a7ddc2a05a98941492af457ae78c95e
Yifan Sun <sunyifan112358@gmail.com>
2017-02-20 22:10:46 -0500

    Change the port to serve

--- server/server.py
+++ server/server.py
@@ -3,6 +3,6 @@
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
-httpd = BaseHTTPServer.HTTPServer(('', 8080), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd = BaseHTTPServer.HTTPServer(('', 18080), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='../server/server.pem', server_side=True)
 httpd.serve_forever()
