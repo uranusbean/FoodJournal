41d98b230ba4a5b4892148954676bac3511118c8
uranusbean <uranusbean@gmail.com>
2017-03-24 13:54:27 -0400

    update placepicker

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -51,7 +51,31 @@ myApp.addView('.view-main', {
 myApp.onPageInit('textfeed', function() {
   currPost = fj.newPost();
   // placepicker
-  $(".placepicker").placepicker();
+  // $(".placepicker").placepicker();
+  // $(function(){
+
+  $("#geocomplete").geocomplete()
+    .bind("geocode:result", function(event, result){
+      $.log("Result: " + result.formatted_address);
+    })
+    .bind("geocode:error", function(event, status){
+      $.log("ERROR: " + status);
+    })
+    .bind("geocode:multiple", function(event, results){
+      $.log("Multiple: " + results.length + " results found");
+    });
+
+  // $("#find").click(function(){
+  //   $("#geocomplete").trigger("geocode");
+  // });
+
+
+  $("#examples a").click(function(){
+    $("#geocomplete").val($(this).text()).trigger("geocode");
+    return false;
+  });
+
+  // });
 
   $("input[data-role=materialtags]").materialtags();
 
