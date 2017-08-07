114cfef86bc61a2f9f2806ca3d64279ce358bf3b
uranusbean <uranusbean@gmail.com>
2017-08-07 15:07:48 -0400

    update tag page selected

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -14,32 +14,8 @@ var $$ = Dom7;
 let currPost;
 let db = new fj.DataBase();
 
-$$('.favoriteBorderIcon').show().on('click',function(){
-  // $$('.favoriteIcon').css('opacity',1);
-  $$('.favoriteIcon').show();
-  // $$('.favoriteBorderIcon').css('opacity', 0);
-  $$('.favoriteBorderIcon').hide();
-});
-
-$$('.favoriteIcon').hide().on('click',function(){
-
-  $$('.favoriteIcon').hide();
-  $$('.favoriteBorderIcon').show();
-});
-
 myApp.onPageInit('index', function() {
-  $$('.favoriteBorderIcon').show().on('click',function(){
-    // $$('.favoriteIcon').css('opacity',1);
-    $$('.favoriteIcon').show();
-    // $$('.favoriteBorderIcon').css('opacity', 0);
-    $$('.favoriteBorderIcon').hide();
-  });
 
-  $$('.favoriteIcon').hide().on('click',function(){
-
-    $$('.favoriteIcon').hide();
-    $$('.favoriteBorderIcon').show();
-  });
 });
 
 // Add view
@@ -48,34 +24,36 @@ myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
-myApp.onPageInit('textfeed', function() {
-  currPost = fj.newPost();
-  // placepicker
-  // $(".placepicker").placepicker();
-  // $(function(){
+let groupTable = {
+    breakfast: {type: 'meal', group:'environment'},
+    lunch: {type: 'meal', group:'environment'},
+    dinner: {type: 'meal', group:'environment'},
+    snack: {type: 'meal', group:'environment'},
 
-  $("#geocomplete").geocomplete()
-    .bind("geocode:result", function(event, result){
-      $.log("Result: " + result.formatted_address);
-    })
-    .bind("geocode:error", function(event, status){
-      $.log("ERROR: " + status);
-    })
-    .bind("geocode:multiple", function(event, results){
-      $.log("Multiple: " + results.length + " results found");
-    });
-  //
-  // $("#find").click(function(){
-  //   $("#geocomplete").trigger("geocode");
-  // });
+    home: {type: 'location', group:'environment'},
+    restaurant: {type: 'location', group:'environment'},
+    eatAtWork: {type: 'location', group:'environment'},
+    snack: {type: 'location', group:'environment'},
 
 
-  $("#examples a").click(function(){
-    $("#geocomplete").val($(this).text()).trigger("geocode");
-    return false;
+
+}
+
+myApp.onPageInit('videoinfo', function() {
+  // tags section goes from here
+  $$('.tagContainer').on('click',function(){
+    $$(this).toggleClass('newtagSelected');
+    let content = $$(this).find('.tagName').text();
+    let subCat =  $$(this).parent().parent().text();
+    // let subCat = $$(this).parent().parent().attr('class');
+    console.log(subCat);
+    console.log(content);
   });
 
-  // });
+});
+
+myApp.onPageInit('textfeed', function() {
+  currPost = fj.newPost();
 
   $("input[data-role=materialtags]").materialtags();
 
@@ -123,79 +101,6 @@ myApp.onPageInit('textfeed', function() {
   });
 });
 
-myApp.onPageInit('textfeed_plate', function() {
-  var count = 0;
-
-  $$('.grainPortion').on('click',function(){
-    count = (count + 1) % 4;
-    if (count === 1) {
-      $$('#grainSmallPortion').css('fill-opacity', '1');
-    } else if (count === 2){
-      $$('#grainMediumPortion').css('fill-opacity', '1');
-    } else if (count === 3){
-      $$('#grainLargePortion').css('fill-opacity', '1');
-    } else if (count === 0){
-      $$('#grainSmallPortion').css('fill-opacity', '0');
-      $$('#grainMediumPortion').css('fill-opacity', '0');
-      $$('#grainLargePortion').css('fill-opacity', '0.1');
-    }
-  });
-
-  $$('.vegePortion').on('click',function(){
-    count = (count + 1) % 4;
-    if (count === 1) {
-      $$('#vegeSmallPortion').css('fill-opacity', '1');
-    } else if (count === 2){
-      $$('#vegeMediumPortion').css('fill-opacity', '1');
-    } else if (count === 3){
-      $$('#vegeLargePortion').css('fill-opacity', '1');
-    } else if (count === 0){
-      $$('#vegeSmallPortion').css('fill-opacity', '0');
-      $$('#vegeMediumPortion').css('fill-opacity', '0');
-      $$('#vegeLargePortion').css('fill-opacity', '0.1');
-    }
-  });
-
-  $$('.fruitsPortion').on('click',function(){
-    count = (count + 1) % 4;
-    if (count === 1) {
-      $$('#fruitsSmallPortion').css('fill-opacity', '1');
-    } else if (count === 2){
-      $$('#fruitsMediumPortion').css('fill-opacity', '1');
-    } else if (count === 3){
-      $$('#fruitsLargePortion').css('fill-opacity', '1');
-    } else if (count === 0){
-      $$('#fruitsSmallPortion').css('fill-opacity', '0');
-      $$('#fruitsMediumPortion').css('fill-opacity', '0');
-      $$('#fruitsLargePortion').css('fill-opacity', '0.1');
-    }
-  });
-
-  $$('.proteinPortion').on('click',function(){
-    count = (count + 1) % 4;
-    if (count === 1) {
-      $$('#proteinSmallPortion').css('fill-opacity', '1');
-    } else if (count === 2){
-      $$('#proteinMediumPortion').css('fill-opacity', '1');
-    } else if (count === 3){
-      $$('#proteinLargePortion').css('fill-opacity', '1');
-    } else if (count === 0){
-      $$('#proteinSmallPortion').css('fill-opacity', '0');
-      $$('#proteinMediumPortion').css('fill-opacity', '0');
-      $$('#proteinLargePortion').css('fill-opacity', '0.1');
-    }
-  });
-
-  $$('#quarter_pies').on('click',function(){
-    // $$('#plateDirection').css('opacity',0);
-    $$('#healthyPlateAdvice').css('opacity',1);
-  });
-
-  $$('.postBtn').on('click', function() {
-    fj.savePost(currPost, db);
-  });
-});
-
 myApp.onPageInit('addvideo', function() {
   currPost = fj.newPost();
   let video = new fj.Video();
@@ -232,85 +137,6 @@ myApp.onPageInit('addvideo', function() {
 
   });
 
-});
-
-myApp.onPageInit('videoinfo', function() {
-  var count = 0;
-
-  $$('.form-to-data').on('click', function(){
-    let formData = myApp.formToData('#my-form');
-    currPost.location = formData.location;
-    currPost.withWhom = formData.withWhom;
-  });
-  
-  $$('.grainPortion').on('click',function(){
-    count = (count + 1) % 4;
-    if (count === 1) {
-      $$('#grainSmallPortion').css('fill-opacity', '1');
-    } else if (count === 2){
-      $$('#grainMediumPortion').css('fill-opacity', '1');
-    } else if (count === 3){
-      $$('#grainLargePortion').css('fill-opacity', '1');
-    } else if (count === 0){
-      $$('#grainSmallPortion').css('fill-opacity', '0');
-      $$('#grainMediumPortion').css('fill-opacity', '0');
-      $$('#grainLargePortion').css('fill-opacity', '0.1');
-    }
-  });
-
-  $$('.vegePortion').on('click',function(){
-    count = (count + 1) % 4;
-    if (count === 1) {
-      $$('#vegeSmallPortion').css('fill-opacity', '1');
-    } else if (count === 2){
-      $$('#vegeMediumPortion').css('fill-opacity', '1');
-    } else if (count === 3){
-      $$('#vegeLargePortion').css('fill-opacity', '1');
-    } else if (count === 0){
-      $$('#vegeSmallPortion').css('fill-opacity', '0');
-      $$('#vegeMediumPortion').css('fill-opacity', '0');
-      $$('#vegeLargePortion').css('fill-opacity', '0.1');
-    }
-  });
-
-  $$('.fruitsPortion').on('click',function(){
-    count = (count + 1) % 4;
-    if (count === 1) {
-      $$('#fruitsSmallPortion').css('fill-opacity', '1');
-    } else if (count === 2){
-      $$('#fruitsMediumPortion').css('fill-opacity', '1');
-    } else if (count === 3){
-      $$('#fruitsLargePortion').css('fill-opacity', '1');
-    } else if (count === 0){
-      $$('#fruitsSmallPortion').css('fill-opacity', '0');
-      $$('#fruitsMediumPortion').css('fill-opacity', '0');
-      $$('#fruitsLargePortion').css('fill-opacity', '0.1');
-    }
-  });
-
-  $$('.proteinPortion').on('click',function(){
-    count = (count + 1) % 4;
-    if (count === 1) {
-      $$('#proteinSmallPortion').css('fill-opacity', '1');
-    } else if (count === 2){
-      $$('#proteinMediumPortion').css('fill-opacity', '1');
-    } else if (count === 3){
-      $$('#proteinLargePortion').css('fill-opacity', '1');
-    } else if (count === 0){
-      $$('#proteinSmallPortion').css('fill-opacity', '0');
-      $$('#proteinMediumPortion').css('fill-opacity', '0');
-      $$('#proteinLargePortion').css('fill-opacity', '0.1');
-    }
-  });
-
-  $$('#quarter_pies').on('click',function(){
-    // $$('#plateDirection').css('opacity',0);
-    $$('#healthyPlateAdvice').css('opacity',1);
-  });
-
-  $$('.postBtn').on('click', function() {
-    fj.savePost(currPost, db);
-  });
 });
 
 myApp.onPageInit('timeline', function() {
