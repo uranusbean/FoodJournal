d8c3c62e937d26dd3a173e82962d3977b91c57a8
uranusbean <uranusbean@gmail.com>
2017-08-08 08:40:59 -0400

    add tag group

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -25,6 +25,7 @@ myApp.addView('.view-main', {
 });
 
 let groupTable = {
+    // Environment
     breakfast: {type: 'meal', group:'environment'},
     lunch: {type: 'meal', group:'environment'},
     dinner: {type: 'meal', group:'environment'},
@@ -32,11 +33,80 @@ let groupTable = {
 
     home: {type: 'location', group:'environment'},
     restaurant: {type: 'location', group:'environment'},
-    eatAtWork: {type: 'location', group:'environment'},
-    snack: {type: 'location', group:'environment'},
+    work: {type: 'location', group:'environment'},
+    fastfood: {type: 'location', group:'environment'},
 
+    alone: {type: 'companion', group:'environment'},
+    friends: {type: 'companion', group:'environment'},
+    family: {type: 'companion', group:'environment'},
+    pet: {type: 'companion', group:'environment'},
 
+    dimmerLight: {type: 'atmospherics', group:'environment'},
+    brighterLight: {type: 'atmospherics', group:'environment'},
+    slowMusic: {type: 'atmospherics', group:'environment'},
+    fastMusic: {type: 'atmospherics', group:'environment'},
+    hot: {type: 'atmospherics', group:'environment'},
+    cold: {type: 'atmospherics', group:'environment'},
+    lightAroma: {type: 'atmospherics', group:'environment'},
+    strongAroma: {type: 'atmospherics', group:'environment'},
 
+    leftover: {type: 'eatingEffort', group:'environment'},
+    takeout: {type: 'eatingEffort', group:'environment'},
+    easycooking: {type: 'eatingEffort', group:'environment'},
+    masterchef: {type: 'eatingEffort', group:'environment'},
+
+    TV: {type: 'distraction', group:'environment'},
+    reading: {type: 'distraction', group:'environment'},
+    computer: {type: 'distraction', group:'environment'},
+    driving: {type: 'distraction', group:'environment'},
+
+    //Food
+    water: {type: 'drink', group:'food'},
+    coffee: {type: 'drink', group:'food'},
+    tea: {type: 'drink', group:'food'},
+    dairy: {type: 'drink', group:'food'},
+    soda: {type: 'drink', group:'food'},
+    juice: {type: 'drink', group:'food'},
+    alcohol: {type: 'drink', group:'food'},
+
+    darkGreenVeg: {type: 'vegetable', group:'food'},
+    starchyVeg: {type: 'vegetable', group:'food'},
+    redOrgangeVeg: {type: 'vegetable', group:'food'},
+    peasBeans: {type: 'vegetable', group:'food'},
+
+    egg: {type: 'protein', group:'food'},
+    poultry: {type: 'protein', group:'food'},
+    seafood: {type: 'protein', group:'food'},
+    tofu: {type: 'protein', group:'food'},
+    nutsSeeds: {type: 'protein', group:'food'},
+    meat: {type: 'protein', group:'food'},
+    peasBeans: {type: 'protein', group:'food'},
+    processedMeat: {type: 'protein', group:'food'},
+
+    wholeGrains: {type: 'grains', group:'food'},
+    refinedGrains: {type: 'grains', group:'food'},
+
+    freshFruit: {type: 'fruit', group:'food'},
+    cannedFruit: {type: 'fruit', group:'food'},
+    driedFruit: {type: 'fruit', group:'food'},
+    frozenFruit: {type: 'fruit', group:'food'},
+
+    // Feelings
+    satisfied: {type: 'positive', group:'feeling'},
+    happy: {type: 'positive', group:'feeling'},
+    peaceful: {type: 'positive', group:'feeling'},
+    energetic: {type: 'positive', group:'feeling'},
+    enthusiastic: {type: 'positive', group:'feeling'},
+    warm: {type: 'positive', group:'feeling'},
+    good: {type: 'positive', group:'feeling'},
+    joyful: {type: 'positive', group:'feeling'},
+
+    disgusted: {type: 'negative', group:'feeling'},
+    bored: {type: 'negative', group:'feeling'},
+    worried: {type: 'negative', group:'feeling'},
+    disppointed: {type: 'negative', group:'feeling'},
+    guilty: {type: 'negative', group:'feeling'},
+    angry: {type: 'negative', group:'feeling'}
 }
 
 myApp.onPageInit('videoinfo', function() {
